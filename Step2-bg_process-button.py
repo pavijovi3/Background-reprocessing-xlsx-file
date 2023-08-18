63032e8fa87cff6c246cbe7ccf5c3902b4c5ec19
pavijovi3 <pavijovi3@gmail.com>
2023-08-18 16:17:51 +0100

    Update Step2-bg_process-button.py

--- Step2-bg_process-button.py
+++ Step2-bg_process-button.py
@@ -10,6 +10,7 @@ import pandas as pd
 # The function then creates a Tkinter window for column selection.
 # The window has a label for column selection, a combobox for column selection, and a button to confirm column selection.
 # When the user clicks the confirm button, the function gets the chosen column and creates a new sheet for processing.
+# Calculate the differences between the selected column and other columns and store the processed data in a DataFrame.
 # The function then prompts the user to select an output directory.
 # The function then gets the input filename without extension, constructs the output file path, and saves the processed sheet to a new workbook.
 # Finally, the function opens the folder where the output files are saved.
